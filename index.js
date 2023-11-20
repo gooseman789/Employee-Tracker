4059fa152e5b797b6e1aae1b88fa66ae2c028863
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2023-11-20 18:37:24 -0500

    working on the some of the functionality of the table

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const inquirer = require ('inquirer')
-const server = require('mysql2')
-const sequelize = require('./connection')
+// const db = require('./connection')
+const mysql = require('mysql2')
 
+// db.connect((err) => {
+//     if (err) {
+//         console.err("Error connecting to database:", err);
+//         return
+//     }
+//     console.log("Connected to the database");
+// })
 
-sequelize.connect((err) => {
-    if (err) {
-        console.err("Error connecting to database:", err);
-        return
-    }
-    console.log("Connected to the database");
-    startMenu()
-})
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'MooseMoose1!',
+      database: 'employeetracker_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
 
 function startMenu () {
     inquirer
         .prompt([
             {
                 type: 'list',
-                name: 'StartMenu',
+                name: 'startMenu',
                 message: "What would you like to do?",
                 choices: ['View All Employees', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
         }
     ])
-        .then((data) => {
+        .then((userdata) => {
+            const data = userdata.startMenu
+            console.log(data)
             if (data == 'View All Employees') {
 
             } else if (data == 'Update Employee Role') {
@@ -30,32 +42,74 @@ function startMenu () {
             } else if (data == 'View All Roles') {
 
             } else if (data == 'Add Role') {
-                inquirer
-                    .prompt([
-                        {
-                            type: 'input',
-                            message: "What Role would you like to add?"
-                        }
-                    ])
-                    .then((data) => {
-
-                    })
+                addRole()
             } else if (data == 'View All Departments') {
-
+                viewDepartments()
             } else {
-                inquirer
-                    .prompt([
-                        {
-                        type: 'input',
-                        message: 'What Department would you like to add?'
-                        }
-                    ])
-                        .then((data) => {
-
-                        })
+                addDepartments()
             }
 
             
         }
         )
     }
+
+    function addRole() {
+        const sql = `SELECT * from department`;
+         db.query(sql, (err, result) => {
+         const department = []
+         for (let i =0; i < result.length; i++) {
+            department.push(
+                {
+                    name: result[i].department_name,
+                    value: result[i].id
+                }
+            )
+         }
+         console.log(department)
+        inquirer
+        .prompt([
+            {
+            type: 'list',
+            name: 'departmentName',
+            message: 'What Department would you like to add?',
+            choices: department
+
+            }])
+            .then((data) => {
+                console.log(data)
+                console.log('add role', data.departmentName.name)
+            })
+         }
+    )}
+
+
+    function addDepartments() {
+        inquirer
+        .prompt([
+            {
+            type: 'input',
+            name: 'name',
+            message: 'What Department would you like to add?'
+            }
+        ])
+            .then((data) => {
+                const sql = `INSERT INTO department (department_name)
+                VALUES (?)`;
+              const params = [data.name];
+              
+              db.query(sql, params, (err, result) => {
+                console.log(result)
+              });
+            })
+        }
+
+    function viewDepartments () {
+        const sql = `SELECT department_name from department`;
+      
+      db.query(sql, (err, result) => {
+        console.table(result)
+      });
+    }
+
+    startMenu()
